74b2dfdd78845be245ad3ee51812a75a70399ddd
Scott Nicholes <scott93gps@gmail.com>
2026-05-22 14:34:45 -0700

    Added ToLines to aid in displaying each DivisionView horizontally.

--- UI/Screens/CueViewScreen.cs
+++ UI/Screens/CueViewScreen.cs
@@ -10,7 +10,7 @@ class CueViewScreen : Screen
     private readonly string _pieceName = "Mode de re";
 
     protected override IViewComponent Header => new HeaderComponent($"Cue {_cueData.Item1}: {_pieceName}");
-    protected override IViewComponent Content => new OptionListView(["1", "2"]);
+    protected override IViewComponent Content => new CueViewContent(_cueData.Item2);
 
     public override Screen? Navigate(string input)
     {
@@ -20,7 +20,7 @@ class CueViewScreen : Screen
 
 class DivisionView : IViewComponent
 {
-    private const int Width = 20;
+    public const int Width = 20;
     private readonly (string DivisionName, List<string> Stops) _division;
 
     public DivisionView((string, List<string>) division)
@@ -36,6 +36,21 @@ class DivisionView : IViewComponent
         foreach (var stop in _division.Stops)
             Console.WriteLine($"{stop}");
     }
+
+    /**
+       Provides the content of this view as a list of horizontal lines.
+     */
+    public List<string> ToLines()
+    {
+        var lines = new List<string>();
+        var divisionName = _division.DivisionName;
+        string centered = divisionName.PadLeft((divisionName.Length + Width) / 2);
+        lines.Add(centered.PadRight(Width));
+        lines.Add(new string('-', Width));
+        foreach (var stop in _division.Stops)
+            lines.Add($"{stop}".PadRight(Width));
+        return lines;
+    }
 }
 
 class CueViewContent : IViewComponent
@@ -50,5 +65,21 @@ class CueViewContent : IViewComponent
 
     public void Render()
     {
+        // Each column is one line of renderable data:
+        // col[0]:  Great             Swell       Pedal
+        // col[1]: --------          --------    --------
+        var columns = _divisions.Select(d => new DivisionView(d).ToLines()).ToList();
+        int height = columns.Max(c => c.Count); // The distance down to loop through
+
+        for (int row = 0; row < height; row++)
+        {
+            foreach (var col in columns)
+            {
+                string line = row < col.Count ? col[row] : new string(' ', DivisionView.Width);
+                Console.Write(line + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
     }
 }
